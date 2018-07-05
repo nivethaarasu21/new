ffb66e4c3a7e05cf3de2d9be99124f995ac6addd
IBM <IBM@IBM-FULLSTACK1.mshome.net>
2018-07-05 17:15:24 +0530

    first

---
+++ Sample/src/test/java/StringCalculatorTest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.hello.StringCalculator;
+
+public class StringCalculatorTest {
+
+	@Test
+	public void testAdd() {
+		StringCalculator.add("1,2");
+	
+	}
+	@Test(expected=RuntimeException.class)
+	public void testIsNumberLengthGreaterThanTwo()
+	{
+		StringCalculator.add("1,2,3");
+	}
+
+}
